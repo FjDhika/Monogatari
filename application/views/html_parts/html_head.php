67015b5d4e6b0104fdd06ad3035bef5135e71c52
Hilda Dea <hildadea3@gmail.com>
2020-04-22 02:08:57 -0700

    add dashboard

--- application/views/html_parts/html_head.php
+++ application/views/html_parts/html_head.php
@@ -2,6 +2,57 @@
 <html>
 <head>
 	<title><?= $page_title ?></title>
+
+	<!-- css dashboard template -->
+    <!-- Fontfaces CSS-->
+    <link href="<?=base_url('assets/backend/css/font-face.css')?>" rel="stylesheet" media="all">
+    <link href="<?=base_url('assets/backend/vendor/font-awesome-4.7/css/font-awesome.min.css')?>" rel="stylesheet" media="all">
+    <link href="<?=base_url('assets/backend/vendor/font-awesome-5/css/fontawesome-all.min.css')?>" rel="stylesheet" media="all">
+    <link href="<?=base_url('assets/backend/vendor/mdi-font/css/material-design-iconic-font.min.css')?>" rel="stylesheet" media="all">
+
+    <!-- Bootstrap CSS-->
+    <link href="<?=base_url('assets/backend/vendor/bootstrap-4.1/bootstrap.min.css')?>" rel="stylesheet" media="all">
+
+    <!-- Vendor CSS-->
+    <link href="<?=base_url('assets/backend/vendor/animsition/animsition.min.css')?>" rel="stylesheet" media="all">
+    <link href="<?=base_url('assets/backend/vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css')?>" rel="stylesheet" media="all">
+    <link href="<?=base_url('assets/backend/vendor/wow/animate.css')?>" rel="stylesheet" media="all">
+    <link href="<?=base_url('assets/backend/vendor/css-hamburgers/hamburgers.min.css')?>" rel="stylesheet" media="all">
+    <link href="<?=base_url('assets/backend/vendor/slick/slick.css')?>" rel="stylesheet" media="all">
+    <link href="<?=base_url('assets/backend/vendor/select2/select2.min.css')?>" rel="stylesheet" media="all">
+    <link href="<?=base_url('assets/backend/vendor/perfect-scrollbar/perfect-scrollbar.css')?>" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+	<link href="<?=base_url('assets/backend/css/theme.css')?>" rel="stylesheet" media="all">
+	<!-- end -->
+
+	<!-- js dashboard template -->
+	<!-- Jquery JS-->
+	<script src="<?=base_url('assets/backend/vendor/jquery-3.2.1.min.js')?>"></script>
+	<!-- Bootstrap JS-->
+	<script src="<?=base_url('assets/backend/vendor/bootstrap-4.1/popper.min.js')?>"></script>
+	<script src="<?=base_url('assets/backend/vendor/bootstrap-4.1/bootstrap.min.js')?>"></script>
+	<!-- Vendor JS       -->
+	<script src="<?=base_url('assets/backend/vendor/slick/slick.min.js')?>">
+	</script>
+	<script src="<?=base_url('assets/backend/vendor/wow/wow.min.js')?>"></script>
+	<script src="<?=base_url('assets/backend/vendor/animsition/animsition.min.js')?>"></script>
+	<script src="<?=base_url('assets/backend/vendor/bootstrap-progressbar/bootstrap-progressbar.min.js')?>">
+	</script>
+	<script src="<?=base_url('assets/backend/vendor/counter-up/jquery.waypoints.min.js')?>"></script>
+	<script src="<?=base_url('assets/backend/vendor/counter-up/jquery.counterup.min.js')?>">
+	</script>
+	<script src="<?=base_url('assets/backend/vendor/circle-progress/circle-progress.min.js')?>"></script>
+	<script src="<?=base_url('assets/backend/vendor/perfect-scrollbar/perfect-scrollbar.js')?>"></script>
+	<script src="<?=base_url('assets/backend/vendor/chartjs/Chart.bundle.min.js')?>"></script>
+	<script src="<?=base_url('assets/backend/vendor/select2/select2.min.js')?>">
+	</script>
+
+	<!-- Main JS-->
+        <script src="<?=base_url('assets/')?>backend/js/main.js"></script>
+	<!-- end -->
+
+
 	<script type="text/javascript" src="<?= base_url('assets/js/jquery-3.4.1.slim.min.js'); ?>"></script>
 	<script type="text/javascript" src="<?= base_url('assets/js/bootstrap-select.js'); ?>"></script>
 	<script type="text/javascript" src="<?= base_url('assets/js/bootstrap.bundle.min.js'); ?>"></script>
