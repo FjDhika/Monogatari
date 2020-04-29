959dac44683e1c2dc8a3fc5ad1e4295371f042f1
Hilda Dea <hildadea3@gmail.com>
2020-04-28 21:41:26 -0700

    Section DashboardView

--- application/views/dashboardView.php
+++ application/views/dashboardView.php
@@ -2,12 +2,103 @@
 
 
 <?php require_once "html_parts/navbar.php" ?>
-<div class="container-fluid">
-
+<div class="featured-story mt-4 mb-3">
 	<div class="container">
-		<h5>ini dashboard</h5>
-		tolotng ini nanti diisi sama list cerita semacam discovery
+		<div class="row">
+
+			<div class="col-md-4">
+				<div class="card mb-4 shadow-sm">
+					<img class="bg-placeholder-img card-img-top" src="" alt="" width="100%" height="225">
+					<div class="card-body">
+						<h4 class="card-header mb-3"> Judul </h4>
+						<div class="d-flex justify-content-between align-items-center">
+							<div class="btn-group">
+								<button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+							</div>
+							<small class="text-muted">9 mins</small>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="card mb-4 shadow-sm">
+					<img class="bg-placeholder-img card-img-top" src="" alt="" width="100%" height="225">
+					<div class="card-body">
+						<h4 class="card-header mb-3"> Judul </h4>
+						<div class="d-flex justify-content-between align-items-center">
+							<div class="btn-group">
+								<button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+							</div>
+							<small class="text-muted">9 mins</small>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-md-4">
+				<div class="card mb-4 shadow-sm">
+					<img class="bg-placeholder-img card-img-top" src="" alt="" width="100%" height="225">
+					<div class="card-body">
+						<h4 class="card-header mb-3"> Judul </h4>
+						<div class="d-flex justify-content-between align-items-center">
+							<div class="btn-group">
+								<button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+							</div>
+							<small class="text-muted">9 mins</small>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="card mb-4 shadow-sm">
+					<img class="bg-placeholder-img card-img-top" src="" alt="" width="100%" height="225">
+					<div class="card-body">
+						<h4 class="card-header mb-3"> Judul </h4>
+						<div class="d-flex justify-content-between align-items-center">
+							<div class="btn-group">
+								<button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+							</div>
+							<small class="text-muted">9 mins</small>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="card mb-4 shadow-sm">
+					<img class="bg-placeholder-img card-img-top" src="" alt="" width="100%" height="225">
+					<div class="card-body">
+						<h4 class="card-header mb-3"> Judul </h4>
+						<div class="d-flex justify-content-between align-items-center">
+							<div class="btn-group">
+								<button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+							</div>
+							<small class="text-muted">9 mins</small>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="card mb-4 shadow-sm">
+					<img class="bg-placeholder-img card-img-top" src="" alt="" width="100%" height="225">
+					<div class="card-body">
+						<h4 class="card-header mb-3"> Judul </h4>
+						<div class="d-flex justify-content-between align-items-center">
+							<div class="btn-group">
+								<button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+							</div>
+							<small class="text-muted">9 mins</small>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
 	</div>
 </div>
+
 
 <?php require_once "html_parts/html_foot.php" ?>
--- application/views/html_parts/html_head.php
+++ application/views/html_parts/html_head.php
@@ -15,5 +15,6 @@
 	<link href="<?= base_url('assets/plugins/dataTable/dataTables.jqueryui.css'); ?>" rel="stylesheet">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="icon" href="<?= base_url('assets/image/logo1.png'); ?>" type="image/x-icon">
+
 </head>
 <body>
