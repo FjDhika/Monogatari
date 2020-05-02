c7b01609c8821bdf014e91d410b2047ec088e458
Cherrydet <cherrydaniella10@gmail.com>
2020-05-03 00:18:02 +0700

    change lil'bit

--- application/views/landingPageView.php
+++ application/views/landingPageView.php
@@ -5,15 +5,15 @@
 <section class="banner">
 	<div class="container">
 		<div class="row">
-			<div class="col-6" style="margin-top: 100px; ">
+			<div class="col-6" style="margin-top: 80px; ">
 				<h2>Monogatari</h2>
-				<p>Platform bagi penulis untuk menerbitkan karyanya dan juga bagi pembaca yang suka membaca tanpa halangan apapun</p>
+				<p>A platform for writers to publish their work and also for readers who like to read without any obstruction</p>
 
 				<button type="button" class="btn btn-lg btn-info" data-toggle="modal" data-target="#register">Let's Read!</button>
 			</div>
 
 			<div class="col-6">
-					<img src="<?= base_url("assets/image/img.jpg") ?>" class="pict">
+					<img src="<?= base_url("assets/image/book.png") ?>" class="pict">
 			</div>
 		</div>
 	</div>
@@ -22,23 +22,42 @@
 
 <section class="tools">
 	<div class="container">
+
+			<h3>"Things You Can Do with Monogatari"</h3>
+
 		<div class="row">
+
 			<div class="col-4">
-				
+				<img src="<?= base_url("assets/image/read.png") ?>">
+				<h2>1. Reading</h2>
+				<p>You can read any book with many categories anywhere anytime without worries</p>
 			</div>
 
 			<div class="col-4">
-				
+				<img src="<?= base_url("assets/image/write.png") ?>">
+				<h2>2. Writing</h2>
+				<p>Share your creation to the world easily and start your own career as a writer, and you can go to public with your own creation</p>
 			</div>
 
 			<div class="col-4">
-				
+				<img src="<?= base_url("assets/image/share.png") ?>">
+				<h2>3. Sharing</h2>
+				<p>Sharing the story that makes your heart puonding to someone else, will make you satisfied. You can recommended and put in your favorite book in your own library also keep reading it as much as you want </p>
 			</div>
 		</div>
 	</div>
 </section>
 
+<section class="book">
+	<div class="container">
 
+		<h3>Our Recommendation</h3>
+
+		<div class="row">
+			
+		</div>
+	</div>
+</section>
 
 
 
