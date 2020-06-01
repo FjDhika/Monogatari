b43b68a43e150eaa7cf6a92cbcc451c006c4b2a8
FJDHIKA <fajarhandika12@gmail.com>
2020-06-01 20:19:31 +0700

    deploy init

--- application/views/html_parts/html_head.php
+++ application/views/html_parts/html_head.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 	<script type="text/javascript" src="<?= base_url('assets/js/jquery.js'); ?>"></script>
-	<script type="text/javascript" src="<?= base_url('assets/js/bootstrap.bundle.min.js'); ?>"></script>
+	<script type="text/javascript" src="<?= base_url('assets/js/bootstrap.js'); ?>"></script>
 	<script type="text/javascript" src="<?= base_url('assets/js/bootstrap-select.js'); ?>"></script>
 	<script type="text/javascript" src="<?= base_url('assets/plugins/dataTable/datatables.js') ?>"></script>
 
@@ -17,7 +17,7 @@
 	<link href="<?= base_url('assets/plugins/dataTable/dataTables.bootstrap4.css'); ?>" rel="stylesheet">
 	<link href="<?= base_url('assets/plugins/dataTable/dataTables.jqueryui.css'); ?>" rel="stylesheet">
 	<link href="<?= base_url('assets/css/cust.css'); ?>" rel="stylesheet">
-	<script src="<?= base_url('assets/fontawesome/js/all.min.js'); ?>"></script>
+	<link type="text/javascript"> src="<?= base_url('assets/fontawesome/js/all.min.js'); ?>"></link>
 	<script src="<?= base_url('assets/fontawesome/css/all.min.css'); ?>"></script>
 	<link rel="icon" href="<?= base_url('assets/image/logo1.png'); ?>" type="image/x-icon">
 
